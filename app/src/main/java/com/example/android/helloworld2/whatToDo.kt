1ca28253d814cd29b5fbd52734252e4180b6c3d1
Kozhukhar Dmitry <dna.coj@gmail.com>
2019-02-27 18:59:06 +0300

    1

--- app/src/main/java/com/example/android/helloworld2/whatToDo.kt
+++ app/src/main/java/com/example/android/helloworld2/whatToDo.kt
@@ -2,6 +2,9 @@ package com.example.android.helloworld2
 
 fun main(){
     fun whatShouldIDoToday(mood: String, weather: String = "sunny", temperature: Int = 24){
-        
+        println(when {
+            mood == "happy" && weather == "Sunny" -> "go for a walk"
+            else -> "Stay home and read."
+        })
     }
 }
